87360873aaa84243949327bfa5578336ac83e9cc
daniel <dmutaka7@gmail.com>
2025-05-03 09:18:56 +0100

    vf2

--- blessed_Foundation/settings.py
+++ blessed_Foundation/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -92,16 +93,20 @@ WSGI_APPLICATION = 'blessed_Foundation.wsgi.application'
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 #}
+ #DATABASES = {
+ #   'default': {
+ #       'ENGINE': 'django.db.backends.mysql',
+ #       'NAME': 'blessed_Foundations',
+ #       'USER': 'root',  # Utilisation de 'USER' au lieu de 'USERNAME'
+ #       'PASSWORD': '',  # Votre mot de passe de base de données MySQL
+ #       'HOST': 'localhost',
+ #       'PORT': '3306',
+ #   }
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blessed_Foundations',
-        'USER': 'root',  # Utilisation de 'USER' au lieu de 'USERNAME'
-        'PASSWORD': '',  # Votre mot de passe de base de données MySQL
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
+
 
 
 # Password validation
